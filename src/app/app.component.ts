84d738640bd4864e2b16d4b2cfffd0e85b35a56d
Fran Diaz B <frandiazb97@gmail.com>
2024-04-24 19:52:10 -0300

    dsadsadsadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'DiazBarbacovi.Francisco';
+  title = 'Calculadora';
+  numeroUno : number = 0;
+  numeroDos : number = 0;
+  total : number = 0;
+  resultado : any;
+  promedio : any;
+  
+
+  ngOnInit(){
+    console.log("Arrancamos");
+  }
+
+  protected OnBtnCalcuar(){
+    this.resultado = "Resultado Total: ";
+    this.promedio = "Promedio: "
+    this.total = this.numeroUno + this.numeroDos;
+
+    this.resultado += this.total;
+    this.promedio += (this.total / 2);
+
+    console.log(this.total);
+  }
+
+  protected OnBtnLimpiar(){
+    this.numeroUno = 0;
+    this.numeroDos = 0;
+    this.resultado = 'Resultado Total: ';
+    this.promedio = "Promedio: "
+  }
 }
